a7a8089a624b9cbc2d049c5fa1649d238132f9d4
jmruiz96 <111460342+jmruiz96@users.noreply.github.com>
2022-09-05 14:54:22 -0500

    timer and start button connected

--- assets/javascript/quiz.js
+++ assets/javascript/quiz.js
@@ -1,1 +1,18 @@
-function 
+var starterButton = document.getElementById("startBtn")
+var timerEl = document.getElementById("counter");
+
+
+var timeLeft = 90;
+
+function countdown() {
+    var timeInterval = setInterval(function() {
+        timerEl.textContent = timeLeft;
+        timeLeft--;
+
+        if(timeLeft === 0) {
+            clearInterval(timeInterval);
+        }
+    }, 1000);
+}
+
+starterButton.addEventListener("click", countdown() )
