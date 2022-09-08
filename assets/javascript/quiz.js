1a7eb89e73a2e59500453b01fb076bb82574fbff
jmruiz96 <111460342+jmruiz96@users.noreply.github.com>
2022-09-07 20:42:18 -0500

    went back to the drawing board, redoing functions for questions

--- assets/javascript/quiz.js
+++ assets/javascript/quiz.js
@@ -6,104 +6,143 @@ var sectionOne = document.getElementById("startPage");
 var sectionTwo = document.getElementById("questionPage");
 var sectionThree = document.getElementById("submissionPage");
 var quizQ = document.getElementById("question");
-var guess = document.querySelectorAll("guesses");
-var questionIndex = 0;
-
+var answers = document.querySelectorAll("#answers");
+var qIndex = 0;
 
 var qArr = [
     {
-        question: "Where should the link to Javascript be located in an HTML file?",
-        choices: [ "Above the head ", "In the head", "Inside and at the top of the body", "Inside and at the bottom of the body"],
+        questionOb: "Where should the link to Javascript be located in an HTML file?",
+        possibleA: [ "Above the head ", "In the head", "Inside and at the bottom of the body", "Inside and at the top of the body", ],
         rightA: "Inside and at the bottom of the body",
     },
 
     {
-        question: "Which operator means not equal to?",
-        choices: [ " = ", " == ", " === ", "!=" ],
+        questionOb: "Which operator means not equal to?",
+        possibleA: [ " = ", " == ", " === ", "!=" ],
         rightA: " != "
     },
 
     { 
-        question: "What do we use to store groups of data in a single variable?",
-        choices: ["Method", "Container", "Div", "Array"],
-        rightA: "Array",
+        questionOb: "What do we use to store groups of data in a single variable?",
+        possibleA: ["Method", "Array", "Container", "Div"],
+        rightA: "Array"
+    },
+
+    {
+        questionOb: "A very useful method used during development and debugging for printing content in dev tools",
+        possibleA: ["this ", "For Loops", "Javascript", "Console.log"], 
+        rightA: "Console.log"
+    },
+
+    {
+        questionOb: "When is the local storage data cleared?",
+        possibleA: ["On computer restart", "On page reload", "On browser close", "No expiration time"], 
+        rightA: "No expiration time"
     },
 ]; 
-//changed wrongAs to choices
-// console.log(qArr[0]["question"]);
+//changed wrongAs to possibleA and question to questionOb
 
 // sectionTwo.children[0].textContent = (qArr[0]["question"])
-// sectionTwo.children[1].children[0].textContent = (qArr[0].wrongAs[0])
-// sectionTwo.children[1].children[1].textContent = (qArr[0].wrongAs[1])
-// sectionTwo.children[1].children[2].textContent = (qArr[0].wrongAs[2])
-// sectionTwo.children[1].children[3].textContent = (qArr[0]["rightA"])
+// sectionTwo.children[1].children[0].textContent = (qArr[0].possibleA[0])
+// sectionTwo.children[1].children[1].textContent = (qArr[0].possibleA[1])
+// sectionTwo.children[1].children[2].textContent = (qArr[0].possibleA[2])
+// sectionTwo.children[1].children[3].textContent = (qArr[0].possibleA[3])
 
-// var index =0;
+// var index = 0;
 // var currentQ;
 
-// function navigate(direction) {
-//     index = index + direction;
-//     if (index <0) {
+// function navigate(next) {
+//     index = index + next;
+//     if (index < 0) {
 //         index = qArr.length - 1;
 //     } else if ( index > qArr.length -1) {
 //         index = 0;
 //     }
 //     currentQ = qArr[index];
-//     sectionTwo.children[0].textContent = (currentQ["question"])
-//     sectionTwo.children[1].children[0].textContent = (currentQ.wrongAs[0])
-//     sectionTwo.children[1].children[1].textContent = (currentQ.wrongAs[1])
-//     sectionTwo.children[1].children[2].textContent = (currentQ.wrongAs[2])
-//     sectionTwo.children[1].children[3].textContent = (currentQ["rightA"])
-// }; trying to target different answers
+//     sectionTwo.children[0].textContent = (qArr[index]["question"]);
+//     sectionTwo.children[1].children[0].textContent = (qArr[index].possibleA[0]);
+//     sectionTwo.children[1].children[1].textContent = (qArr[index].possibleA[1]);
+//     sectionTwo.children[1].children[2].textContent = (qArr[index].possibleA[2]);
+//     sectionTwo.children[1].children[3].textContent = (qArr[index].possibleA[3]);
+// };
 
-function nextQuestion (){
-    var current = qArr[questionIndex]
+// function pickingA (event) {
+//     event.currentTarget.setAttribute()
+// }
+// all this just not working starting over with showQuestions function.
 
-    var answer1 = document.getElementById('answer1')
-    var answer2 = document.getElementById('answer2')
-    var answer3 = document.getElementById('answer3')
-    var answer4 = document.getElementById('answer4')
+function showQuestions() {
+    var currentQ = qArr[qIndex];
+    quizQ.textContent = currentQ.questionOb;
+    answers.innerHTML = "";
+    for (var i = 0; i < currentQ.possibleA.length; i++){
+        var answerBtn = document.createElement("button");
+        answerBtn.setAttribute("class", "btn");
+        answerBtn.textContent = currentQ.possibleA[i];
+        answers.appendChild(answerBtn);
+    };
+};
 
-    quizQ.textContent = current.qArr
+var answerEl = document.getElementById("answers");
+answerEl.addEventListener("click", function answerClick(e){
+    e.preventDefault();
+    if (!e.target.matches("button"))
+    return;
+    var userPick = e.target.textContent;
+    var question = qArr[qIndex];
+    var correct = question.possibleA[question.rightA];
 
-    answer1.textContent = current.choices[0]
-    answer2.textContent = current.choices[1]
-    answer3.textContent = current.choices[2]
-    answer4.textContent = current.choices[3]
+    if (userPick === correct) {
+        timeLeft +=2;
+        resultEl.style.display = "block";
+        resultEl.textContent = "Correct :)";
+    } else {
+        timeLeft -=8;
+        resultEl.style.display = "block";
+        resultEl.textContent = "Wrong :(";
+    }
+})
 
-    document.querySelectorAll('.option').forEach(function (answerEl){
-        answerEl.addEventListener('click', checkAnswer)
-    })
-}
+
+
+
+// answer.addEventListener("click", function(event){
+//     event.stopPropigatiion();
+//     navigate(1)
+
+// });
 
 function countdown() {
-    var timeLeft = 9;
+    var timeLeft = 90;
 
     var timeInterval = setInterval(function() {
         timerEl.textContent = timeLeft;
         timeLeft--;
 
 
-    sectionOne.setAttribute("class", "card-hidden");
-    sectionTwo.setAttribute("class", "card-revealed");
+    // sectionOne.setAttribute("class", "card-hidden");
+    // sectionTwo.setAttribute("class", "card-revealed");
                  
         if(timeLeft < 0) {
             clearInterval(timeInterval);
             // if (step === "two") {
             //     step = "three";
             // not needed
-                sectionTwo.setAttribute("class", "card-hidden");
-                sectionThree.setAttribute("class", "car-revealed");
-            // }
+        //         sectionTwo.setAttribute("class", "card-hidden");
+        //         sectionThree.setAttribute("class", "car-revealed");
+        //     // }
         }
     }, 1000);
-}
+    // navigate(0); not going this route going to make a display q function
+    showQuestions();
+};
+
+
+
 // var step = "one"
 // not needed
 
 
-
-starterButton.addEventListener("click", countdown);
 // starterButton.addEventListener("click", function() {
 //     if (step === "one") {
 //         step = "two";
@@ -111,6 +150,5 @@ starterButton.addEventListener("click", countdown);
 //         sectionTwo.setAttribute("class", "card-revealed") 
 //     }
 // } );
-// not needed
-
-guess.addEventListener("click", )
+// not needed;
+starterButton.addEventListener("click", countdown);
